9ef60cf37e7aaf073841ec3e4b59e77273019efc
julfinch <julfinch@gmail.com>
2022-03-19 01:10:05 +0800

    v1.2

--- email-validation.js
+++ email-validation.js
@@ -4,13 +4,13 @@ var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(inputText.value.match(mailformat))
 {
 document.getElementById("emailValidationMessage").innerHTML = "Success!";
-document.form1.text1.focus();
+document.form1.email.focus();
 return true;
 }
 else
 {
 document.getElementById("emailValidationMessage").innerHTML = "Please enter a valid email address";
-document.form1.text1.focus();
+document.form1.email.focus();
 return false;
 }
 }
